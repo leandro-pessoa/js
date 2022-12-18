a3252c880da129ee510bd00abc13839d7386eaed
leandro-pessoa <119120060+leandro-pessoa@users.noreply.github.com>
2022-12-18 19:12:32 -0300

    alterações no menu js

--- math/script/principal.js
+++ math/script/principal.js
@@ -3,6 +3,10 @@ let size = '300px';
 let open = true;
 let nav = document.getElementById("nav");
 let fechar = document.querySelector("button#fechar");
+let content = document.querySelector("span.nav-content");
+function block(){
+    content.style.display = 'block';
+}
 but.addEventListener('click', e => {
     open = !open;
     if(open == true){
@@ -11,10 +15,13 @@ but.addEventListener('click', e => {
 })
 but.addEventListener('click', e => {
     if(open == false){
+        setTimeout(block, 1000*0.13);
         nav.style.width = `${size}`;
     }
+    content.style.display = 'none';
 })
 fechar.addEventListener('click', e => {
+    content.style.display = 'none'
     nav.style.width = 0;
     open = !open;
 })
