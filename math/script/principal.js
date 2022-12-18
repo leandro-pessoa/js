def7e8a6fc898e0c9697b1b47001738fe4a81ecd
leandro-pessoa <119120060+leandro-pessoa@users.noreply.github.com>
2022-12-18 16:26:42 -0300

    menu js completado

--- math/script/principal.js
+++ math/script/principal.js
@@ -2,6 +2,7 @@ let but = document.getElementById("but");
 let size = '300px';
 let open = true;
 let nav = document.getElementById("nav");
+let fechar = document.querySelector("button#fechar");
 but.addEventListener('click', e => {
     open = !open;
     if(open == true){
@@ -12,5 +13,9 @@ but.addEventListener('click', e => {
     if(open == false){
         nav.style.width = `${size}`;
     }
+})
+fechar.addEventListener('click', e => {
+    nav.style.width = 0;
+    open = !open;
 })
 
