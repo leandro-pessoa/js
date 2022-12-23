cee80b902cbd94b62cb6d9012510ef6212466d99
leandro-pessoa <119120060+leandro-pessoa@users.noreply.github.com>
2022-12-23 19:51:10 -0300

    finalização do projeto agenda

--- menu-js/projetos/agenda/agenda.js
+++ menu-js/projetos/agenda/agenda.js
@@ -1,27 +1,91 @@
-let txt = document.querySelector('textarea#itxt');
-let ad = document.querySelector('button#ad');
-let div = document.querySelector('div#content');
-let apagar = document.querySelector('button#lixo');
-let box = document.querySelector('span.texto');
-ad.addEventListener('click', e => {
-    if (txt.value.length == 0){
+
+let getDados = () => JSON.parse(localStorage.getItem('todoList')) ?? [];
+let setDados = (banco) => localStorage.setItem('todoList', JSON.stringify(banco));
+let criarSave = (tarefa, stat='', indice) => {
+    let item = document.createElement('label');
+    item.classList.add('label');
+    item.innerHTML = `
+    <article id="save">
+    <input type="checkbox" ${stat} data-indice=${indice}>
+    <span class="trash-box">
+        <button data-indice=${indice} type="button">X</button>
+    </span>
+    <span class="texto">${tarefa}</span>
+    `;
+    document.querySelector('div#content').appendChild(item);
+};
+let limparTarefas = () => {
+    let container = document.querySelector('div#content');
+    while (container.firstChild){
+        container.removeChild(container.lastChild);
+    }
+}
+
+let render = () => {
+    limparTarefas();
+    let dados = getDados();
+    dados.forEach((item, indice) => criarSave(item.tarefa, item.stat, indice));
+}
+
+let adItem = (evento) => {
+    let dados = getDados();
+    let tecla = evento.key;
+    let txt = document.querySelector('input#itxt');
+    if(tecla === 'Enter'){  
+        if(txt.value == 0){
+            alert('Insira valores antes de prosseguir!')
+        }
+        else{
+            let dados = getDados(); 
+            dados.push({"tarefa" : txt.value, "stat" : ""});
+            setDados(dados);
+            txt.value = '';
+            txt.focus();
+            render();
+        }
+    }
+};
+
+let removerItem = (indice) => {
+    let dados = getDados();
+    dados.splice (indice,1);
+    setDados(dados);
+    render();
+};
+let atualizarItem = (indice) => {
+    let dados = getDados();
+    dados[indice].stat = dados[indice].stat === 'checked' ? '' : 'checked';
+    setDados(dados);
+    render();
+}
+let clickItem = (evento) => {
+    let element = evento.target;
+    if (element.type === 'button'){
+        let indice = element.dataset.indice;
+        removerItem(indice);
+    }
+    else if(element.type === 'checkbox'){
+        let indice = element.dataset.indice;
+        atualizarItem(indice);
+    }
+};
+let txt = document.querySelector('input#itxt');
+txt.addEventListener('keypress', adItem);
+document.querySelector('div#content').addEventListener('click', clickItem);
+
+document.querySelector('button#ad').addEventListener('click', e => {
+    if (txt.value == 0){
         alert('Insira valores antes de prosseguir!');
     }
     else{
-        div.innerHTML += `<article id="save">
-        <span class="trash-box">
-            <button id="lixo">
-                <img src="imagens/delete-png.png" alt="Apagar item fixado">
-            </button>
-        </span>
-        <span class="texto">
-            ${txt.value}
-        </span>
-    </article>`;
+        let dados = getDados(); 
+        dados.push({"tarefa" : txt.value, "stat" : ""});
+        setDados(dados);
         txt.value = '';
         txt.focus();
+        render();
     }
 });
-apagar.addEventListener('click', e => {
-    
-})
+apear();
+render();
+
