cba3f2ad0258b0561a93f41baab537e38ce70a6c
leandro-pessoa <119120060+leandro-pessoa@users.noreply.github.com>
2022-12-26 18:00:00 -0300

    finalização do projeto piano

--- menu-js/projetos/piano/piano.js
+++ menu-js/projetos/piano/piano.js
@@ -1,1 +1,42 @@
+let keys = document.querySelectorAll('.key');
 
+const tocarNota = (evento) => {
+    let audioKeyCode = getKeyCode(evento);
+    const key = document.querySelector(`.key[data-key="${audioKeyCode}"]`);
+    const keyExists = key;
+    if(!keyExists){
+        return;
+    }
+    efeito(key);
+    playAudio(audioKeyCode);
+}
+const getKeyCode = (evento) => {
+    let keyCode;
+    const isKey = evento.type === 'keydown';
+    if(isKey){
+        keyCode = evento.keyCode;
+    }
+    else{
+        keyCode = evento.target.dataset.key;
+    }
+    return keyCode;
+}
+const efeito = (key) => {
+    key.classList.add('tocar');
+}
+const retirarEfeito = (evento) => {
+    evento.target.classList.remove('tocar');
+}
+const playAudio = (audioKeyCode) => {
+    const audio = document.querySelector(`audio[data-key="${audioKeyCode}"]`);
+    audio.currentTime = 0;
+    audio.play();
+}
+function init(){
+    keys.forEach(function(key){
+        key.addEventListener('click', tocarNota);
+        key.addEventListener('transitionend', retirarEfeito);
+    })
+    window.addEventListener('keydown', tocarNota);
+}
+window.addEventListener('load', init);
